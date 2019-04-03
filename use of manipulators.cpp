a9d7c812b6addae2d66a81fb1c8aa19d7594ca90
aryachiranjeev <31439731+aryachiranjeev@users.noreply.github.com>
2019-04-03 23:07:32 +0530

    Add files via upload

---
+++ use of manipulators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip> // for setw
+using namespace std;
+
+int main(){
+
+int basic=950,allowance=95,total=1045;
+cout<<setw(10)<<"basic"<<setw(10)<<basic<<endl;
+cout<<setw(10)<<"allowance"<<setw(10)<<allowance<<endl;
+cout<<setw(10)<<"total"<<setw(10)<<total<<endl;
+return 0;
+}
+/*output:
+   basic       950
+ allowance        95
+     total      1045
+
+
+*/
